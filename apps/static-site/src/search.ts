dc420c007f57b4d59a7fabe5eb405c2ec0f5b6f5
Stephen Fraser <stephen.fraser@digirati.com>
2024-07-15 15:55:30 +0100

    Set default environment variables to main

--- apps/static-site/src/search.ts
+++ apps/static-site/src/search.ts
@@ -1,13 +1,13 @@
 import TypesenseInstantSearchAdapter from "typesense-instantsearch-adapter";
 import manifestSchema from "@repo/iiif/build/meta/typesense/manifests.schema.json";
 
-const TYPESENSE_API_KEY = process.env["NEXT_PUBLIC_TYPESENSE_API_KEY"] || "xyz";
-const TYPESENSE_HOST = process.env["NEXT_PUBLIC_TYPESENSE_HOST"] || "localhost";
+const TYPESENSE_API_KEY = process.env["NEXT_PUBLIC_TYPESENSE_API_KEY"] || "8EOitKCMTbxUKPZNqUEoQS9M2RGvpkZS";
+const TYPESENSE_HOST = process.env["NEXT_PUBLIC_TYPESENSE_HOST"] || "63flhve71t2un5xgp.a1.typesense.net";
 const TYPESENSE_PORT = process.env["NEXT_PUBLIC_TYPESENSE_PORT"]
   ? parseInt(process.env["NEXT_PUBLIC_TYPESENSE_PORT"])
-  : 8108;
-const TYPESENSE_PROTOCOL = process.env["NEXT_PUBLIC_TYPESENSE_PROTOCOL"] || "http";
-const TYPESENSE_COLLECTION_NAME = process.env["NEXT_PUBLIC_TYPESENSE_COLLECTION_NAME"] || "manifests";
+  : 443;
+const TYPESENSE_PROTOCOL = process.env["NEXT_PUBLIC_TYPESENSE_PROTOCOL"] || "https";
+const TYPESENSE_COLLECTION_NAME = process.env["NEXT_PUBLIC_TYPESENSE_COLLECTION_NAME"] || "production-manifests";
 
 export const typesenseServerConfig = {
   apiKey: TYPESENSE_API_KEY,
