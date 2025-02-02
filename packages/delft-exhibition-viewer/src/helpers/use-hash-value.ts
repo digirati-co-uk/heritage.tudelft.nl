748486c080454148ed0f3de06f7a02826bf12a15
Stephen Fraser <stephen.fraser@digirati.com>
2025-02-02 21:08:35 +0000

    Saved use-hash into store

--- packages/delft-exhibition-viewer/src/helpers/use-hash-value.ts
+++ packages/delft-exhibition-viewer/src/helpers/use-hash-value.ts
@@ -1,12 +1,25 @@
 import mitt from "mitt";
 import { useCallback, useEffect, useState } from "react";
+import { create } from "zustand";
 
 export const tableOfContentsEmitter = mitt<{
   change: string;
 }>();
 
+const useStore = create<{
+  hashValue: string | null;
+  setHashValue: (hashValue: string) => void;
+}>((set, get) => {
+  return {
+    hashValue: null as string | null,
+    setHashValue: (hashValue: string) => {
+      set({ hashValue });
+    },
+  };
+});
+
 export function useHashValue(onChange?: (hashValue: string) => void) {
-  const [hashValue, _setHashValue] = useState<string | null>(null);
+  const { hashValue, setHashValue: _setHashValue } = useStore();
 
   const setHashValue = useCallback((hashValue: string) => {
     if (hashValue) {
