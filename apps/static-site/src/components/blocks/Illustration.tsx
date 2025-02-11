e39d9f2880e95e2228c0960cc46205de148700d3
Stephen Fraser <stephen.fraser@digirati.com>
2025-02-11 14:18:35 +0000

    Added publication manfiest workflow

--- apps/static-site/src/components/blocks/Illustration.tsx
+++ apps/static-site/src/components/blocks/Illustration.tsx
@@ -1,24 +1,50 @@
 import { ManifestLoader } from "@/app/provider";
 import { loadManifest } from "@/iiif";
-import { ObjectViewer } from "../iiif/ObjectViewer";
 import { createThumbnailHelper } from "@iiif/helpers/thumbnail";
-import { AutoLanguage } from "../pages/AutoLanguage";
 import imageServiceLinks from "@repo/iiif/build/meta/image-service-links.json";
+import { DevCreateManifest } from "../atoms/DevCreateManifest";
+import { DevEditManifest } from "../atoms/DevEditManifest";
+import { ObjectViewer } from "../iiif/ObjectViewer";
+import { AutoLanguage } from "../pages/AutoLanguage";
 
 // @todo make this a page block with the box loader.
 export async function Illustration(props: { source: string }) {
   const uuid = props.source.split(".json")[0];
   const slug = `manifests/${uuid}`;
+
+  // Check if it exists.
+  try {
+    await loadManifest(`/${slug}`);
+  } catch (e) {
+    // Does not exist yet. OR display none.
+    return <DevCreateManifest slug={slug} />;
+  }
+
   const { manifest: data } = await loadManifest(`/${slug}`);
 
   const thumbnailHelper = createThumbnailHelper();
-  const thumb = await thumbnailHelper.getBestThumbnailAtSize(data, { width: 512, height: 512 });
+  const thumb = await thumbnailHelper.getBestThumbnailAtSize(data, {
+    width: 512,
+    height: 512,
+  });
   const links = imageServiceLinks[slug as keyof typeof imageServiceLinks] || [];
   const link = links[0] || null;
 
+  if (data.items.length === 0) {
+    if (process.env.NODE_ENV === "production") {
+      return null;
+    }
+
+    return (
+      <div className="bg-white/50 p-8 gap-3 h-96 cut-corners flex-col flex items-center justify-center">
+        <DevEditManifest slug={slug} />
+      </div>
+    );
+  }
+
   return (
     <ManifestLoader manifest={{ ...data }}>
-      <div className="mb-8 grid-cols-7 md:grid">
+      <div className="mb-8 grid-cols-7 md:grid relative group">
         <div className="cut-corners col-span-4 aspect-square bg-black">
           <ObjectViewer
             className="h-full w-full cursor-pointer"
@@ -43,6 +69,11 @@ export async function Illustration(props: { source: string }) {
             <AutoLanguage>{data.summary}</AutoLanguage>
           </div>
         </div>
+        {process.env.NODE_ENV !== "production" ? (
+          <div className="bg-white p-3 rounded transition-opacity group-hover:opacity-100 opacity-0 absolute bottom-3 right-3">
+            <DevEditManifest slug={slug} />
+          </div>
+        ) : null}
       </div>
     </ManifestLoader>
   );
