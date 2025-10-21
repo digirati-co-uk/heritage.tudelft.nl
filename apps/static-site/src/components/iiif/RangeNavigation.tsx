ec3003371e88f357ac46f306cb78aa129120996d
funkydunc <duncan.saunders@digirati.com>
2025-10-21 10:48:38 +0100

    use iiif helpers

--- apps/static-site/src/components/iiif/RangeNavigation.tsx
+++ apps/static-site/src/components/iiif/RangeNavigation.tsx
@@ -1,6 +1,10 @@
-import { useSimpleViewer } from "react-iiif-vault";
+import { useSimpleViewer, useVault, useManifest } from "react-iiif-vault";
 import type { Manifest } from "@iiif/presentation-3";
-import { getValue } from "@iiif/helpers";
+import {
+  getValue,
+  rangesToTableOfContentsTree,
+  type RangeTableOfContentsNode,
+} from "@iiif/helpers";
 
 type RangeNavigationProps = {
   manifest: Manifest;
@@ -8,18 +12,19 @@ type RangeNavigationProps = {
 export function RangeNavigation({ manifest }: RangeNavigationProps) {
   const context = useSimpleViewer();
   const { setCurrentCanvasId } = context;
-  const ranges = manifest.structures?.filter(
-    (struct) => struct.type === "Range",
-  );
+  const vault = useVault();
+  const mani = useManifest();
+  const ranges = mani?.structures?.filter((struct) => struct.type === "Range");
+  const toc = rangesToTableOfContentsTree(vault, ranges);
 
   return (
     <div className="overflow-hidden font-mono">
       <div className="cut-corners w-full place-self-start bg-black p-5 text-white">
         <h3 className="mb-4 uppercase">Table of Contents</h3>
-        {ranges?.map((range) => {
+        {toc?.items?.map((range: RangeTableOfContentsNode) => {
           return (
             <div key={range?.id}>
-              {range.items?.map((item) => {
+              {range.items?.map((item: RangeTableOfContentsNode) => {
                 return (
                   <button
                     key={item.id}
