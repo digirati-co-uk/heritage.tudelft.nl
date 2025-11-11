f7ce0f0448c72ae897f183be0438a367020790c3
funkydunc <duncan.saunders@digirati.com>
2025-11-11 14:52:59 +0000

    translations

--- apps/static-site/src/app/global-not-found.tsx
+++ apps/static-site/src/app/global-not-found.tsx
@@ -1,6 +1,10 @@
 import { GlobalFooter } from "@/components/GlobalFooter";
 import { GlobalHeader } from "@/components/GlobalHeader";
-import { NextIntlClientProvider, useMessages } from "next-intl";
+import {
+  NextIntlClientProvider,
+  useMessages,
+  useTranslations,
+} from "next-intl";
 import "./globals.css";
 import localFont from "next/font/local";
 
@@ -55,28 +59,36 @@ const foundersGroteskMono = localFont({
   ],
 });
 
-export default function GlobalNotFound() {
+export default async function GlobalNotFound({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  //const t = useTranslations();
+  const t = (arg: string) => arg;
   return (
-    <html>
+    <html lang={locale}>
       <body>
-        <NextIntlClientProvider>
+        <NextIntlClientProvider locale={locale}>
           <div
             className={`bg-gray-200 ${foundersGrotesk.variable} ${foundersGroteskMono.variable} font-sans flex flex-col max-h-[100vh] min-h-[100vh] justify-between`}
           >
             <GlobalHeader />
             <main className="flex w-full flex-col items-center grow">
               <div className="w-full max-w-screen-xl px-5 py-10 lg:px-10 flex flex-col gap-2 prose md:prose-xl">
-                <h2>Not found (404)</h2>
-                <span>The page you requested has not been found.</span>
+                <h2>{t("Not found")} (404)</h2>
+                <span>{t("The page you requested has not been found")}.</span>
                 <span>
-                  Please use the navigation above to browse or search the
-                  site.{" "}
+                  {t(
+                    "Please use the navigation above to browse or search the site",
+                  )}
+                  .
                 </span>
                 <span>
                   <a href="" className="underline">
-                    Click here
-                  </a>{" "}
-                  to try to reload this page.
+                    {t("Reload this page")}.
+                  </a>
                 </span>
               </div>
             </main>
