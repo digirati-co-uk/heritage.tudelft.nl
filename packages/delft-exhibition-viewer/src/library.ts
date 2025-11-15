44bf6bb193560ef97fdf0f01555d4267fd5540ea
Stephen Fraser <stephen.fraser@digirati.com>
2025-11-15 16:55:58 +0000

    Updated exports for exhibition viewer

---
+++ packages/delft-exhibition-viewer/src/library.ts
@@ -0,0 +1,68 @@
+// Components
+export * from "./components/CanvasPreviewBlock";
+export * from "./components/RenderSeeAlso";
+export * from "./components/TableOfContentsModal";
+export * from "./components/ViewerZoomControls";
+export * from "./components/VisibleAnnotationListItem";
+
+// Exhibition components
+export * from "./components/exhibition/BaseExhibitionBlock";
+export * from "./components/exhibition/ImageBlock";
+export * from "./components/exhibition/InfoBlock";
+export * from "./components/exhibition/MediaBlock";
+export * from "./components/exhibition/TitleBlock";
+
+// Icons
+export * from "./components/icons/BurgerIcon";
+export * from "./components/icons/CloseIcon";
+export * from "./components/icons/CollapseUpIcon";
+export * from "./components/icons/ContentsIcon";
+export * from "./components/icons/DownIcon";
+export * from "./components/icons/ExpandDownIcon";
+export * from "./components/icons/IIIFIcon";
+export * from "./components/icons/InfoIcon";
+export * from "./components/icons/NextIcon";
+export * from "./components/icons/PauseIcon";
+export * from "./components/icons/PlayIcon";
+export * from "./components/icons/PreviousIcon";
+export * from "./components/icons/TopIcon";
+export * from "./components/icons/UpIcon";
+
+// Presentation components
+export * from "./components/presentation/CanvasPresentationBlock";
+export * from "./components/presentation/ImageBlockPresentation";
+export * from "./components/presentation/InfoBlockPresentation";
+export * from "./components/presentation/MediaBlockPresentation";
+export * from "./components/presentation/TitleBlockPresentation";
+
+// Shared components
+export * from "./components/shared/BaseGridSection";
+export * from "./components/shared/BaseSlide";
+export * from "./components/shared/IIIFMediaPlayer";
+export * from "./components/shared/InfoBlockContents";
+export * from "./components/shared/ReadMore";
+export * from "./components/shared/TableOfContents";
+export * from "./components/shared/TableOfContentsBar";
+export * from "./components/shared/TableOfContentsHeader";
+
+// Main components
+export * from "./DelftExhibition";
+export * from "./DelftPresentation";
+export * from "./DelftSlideshow";
+
+// Helpers
+export * from "./helpers/exhibition-store";
+export * from "./helpers/exhibition";
+export * from "./helpers/get-items-by-locale";
+export * from "./helpers/MapCanvases";
+export * from "./helpers/MapCanvasStrategy";
+export * from "./helpers/object-links";
+export * from "./helpers/use-canvas-highlights";
+export * from "./helpers/use-hash-value";
+export * from "./helpers/use-intersection-ref";
+export * from "./helpers/use-step-details";
+export * from "./helpers/with-view-transition";
+
+// Hooks
+export * from "./hooks/use-exhibition-store";
+export * from "./hooks/use-info-box-contents";
