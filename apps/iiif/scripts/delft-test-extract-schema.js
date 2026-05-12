3d903cf74f52eaef18e8f2894b335a1b6a8773ae
Jules Schoonman <jules.schoonman@gmail.com>
2026-05-12 12:33:39 +0200

    extract topics from schema.json

--- apps/iiif/scripts/delft-test-extract-schema.js
+++ apps/iiif/scripts/delft-test-extract-schema.js
@@ -1,6 +1,10 @@
 import { getValue } from "@iiif/helpers";
 import { extract } from "iiif-hss";
 
+function getValueAsArray(prop) {
+  return Array.isArray(prop) ? prop : prop === undefined ? [] : [prop];
+}
+
 extract(
   {
     id: "delft-test-extract-schema",
@@ -24,9 +28,38 @@ extract(
     const schema = await api.resourceFiles.loadJson("schema.json");
     if (!schema) return {};
 
+    const format = [schema.exampleOfWork.name];
+
+    const contributor = [];
+    if (schema.creator) {
+      getValueAsArray(schema.creator).forEach((role) => {
+        contributor.push(role.creator.name);
+      });
+    }
+    if (schema.contributor) {
+      getValueAsArray(schema.contributor).forEach((role) => {
+        contributor.push(role.contributor.name);
+      });
+    }
+
+    let material = [];
+    if (schema.material) {
+      material = getValueAsArray(schema.material).map(
+        (material) => material.name,
+      );
+    }
+
+    let date = [];
+    if (schema.temporalCoverage) {
+      date = schema.temporalCoverage.split("/");
+    }
+
     return {
       indices: {
-        exampleOfWork: [schema.exampleOfWork.name],
+        format,
+        contributor,
+        material,
+        date,
       },
       search: {
         record: {
