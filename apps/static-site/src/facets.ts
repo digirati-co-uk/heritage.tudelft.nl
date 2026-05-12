a473cc335bed221de0dbfc63805ee7d159b1a60e
Jules Schoonman <jules.schoonman@gmail.com>
2026-05-12 12:34:04 +0200

    updated facets labels & order

--- apps/static-site/src/facets.ts
+++ apps/static-site/src/facets.ts
@@ -2,18 +2,26 @@ import type { InternationalString } from "@iiif/presentation-3";
 
 // This is the code for configuring facets.
 export const facetConfig = {
-  exclude: ["contributor"],
+  exclude: [],
 
-  order: ["exampleOfWork", "material"],
+  order: ["format", "contributor", "material", "date"],
 
   metadata: {
     // Override labels.
-    exampleOfWork: {
+    format: {
       label: { en: ["Object name"], nl: ["Objectnaam"] },
+      interactive: true,
     },
     material: {
       label: { en: ["Material"], nl: ["Materiaal"] },
       interactive: true,
+    },
+    contributor: {
+      label: { en: ["Maker"], nl: ["Maker"] },
+      interactive: true,
+    },
+    date: {
+      label: { en: ["Year"], nl: ["Jaar"] },
     },
   } as Record<string, { label: InternationalString; interactive?: boolean }>,
 };
